dfa91cf050b41ca1e87326bb01722187839d0de4
Ruslan Knyazev <rk@megaperfume.ru>
2019-05-04 13:02:04 +0500

    Add null constraint for question body, title and answer title

---
+++ db/migrate/20190504075942_change_columns_null_in_answers_and_questions.rb
@@ -0,0 +1,7 @@
+class ChangeColumnsNullInAnswersAndQuestions < ActiveRecord::Migration[5.2]
+  def change
+    change_column_null :questions, :title, false
+    change_column_null :questions, :body, false
+    change_column_null :answers, :body, false
+  end
+end
