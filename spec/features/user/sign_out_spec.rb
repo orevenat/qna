5b24be6f61145d2c797b64dc89f3a439bd116cb0
Ruslan Knyazev <agrry@yandex.ru>
2019-06-30 17:42:04 +0500

    user can logout, sign in, sign up

---
+++ spec/features/user/sign_out_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+feature 'User can sign out', %q{
+  As an authenticated user
+  I'd like to be able to sign out
+} do
+
+  given(:user) { create(:user) }
+
+  background do
+    sign_in(user)
+    visit root_path
+  end
+
+  scenario 'Registered user can sign out' do
+    click_on 'Log out'
+
+    expect(page).to have_content 'Signed out successfully.'
+  end
+end
