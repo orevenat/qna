705a02f2400d43fb24b20f7e7c1ec0cc2cf916ce
Ruslan Knyazev <agrry@yandex.ru>
2019-08-17 16:55:19 +0500

    user can add links when he edit his question and asnwer

--- spec/features/answer/edit_spec.rb
+++ spec/features/answer/edit_spec.rb
@@ -9,6 +9,7 @@ feature 'User can edit his answer', %q{
   given!(:user) { create(:user) }
   given!(:user2) { create(:user) }
   given!(:question) { create(:question) }
+  given(:link_url) { 'https://google.com' }
   given!(:answer) { create(:answer, question: question, user: user) }
   given!(:answer2) { create(:answer, question: question, user: user2) }
 
@@ -23,44 +24,54 @@ feature 'User can edit his answer', %q{
       visit question_path(question)
     end
 
-    scenario 'edits his answer' do
-      click_on 'Edit answer'
-
-      within "#answer-#{answer.id}" do
-        fill_in 'Your answer', with: 'edited answer'
-        click_on 'Save answer'
-
-        expect(page).to_not have_content answer.body
-        expect(page).to have_content 'edited answer'
-        expect(page).to_not have_selector 'textarea'
-      end
-    end
-
-    scenario 'edit his answer with attached files' do
-      click_on 'Edit answer'
-
-      within "#answer-#{answer.id}" do
-        fill_in 'Your answer', with: 'edited answer'
-        attach_file 'Files', ["#{Rails.root}/spec/rails_helper.rb", "#{Rails.root}/spec/spec_helper.rb"]
-        click_on 'Save answer'
-
-        expect(page).to have_link 'rails_helper.rb'
-        expect(page).to have_link 'spec_helper.rb'
-      end
-    end
-
-    scenario 'edits his answer with errors' do
-      click_on 'Edit answer'
-
-      within "#answer-#{answer.id}" do
-        fill_in 'Your answer', with: ''
-        click_on 'Save answer'
-
-        expect(page).to have_content answer.body
-        expect(page).to have_content "Body can't be blank"
-        expect(page).to have_selector 'textarea'
+    context 'user try edit' do
+      background do
+        click_on 'Edit answer'
       end
 
+      scenario 'edits his answer' do
+        within "#answer-#{answer.id}" do
+          fill_in 'Your answer', with: 'edited answer'
+          click_on 'Save answer'
+
+          expect(page).to_not have_content answer.body
+          expect(page).to have_content 'edited answer'
+          expect(page).to_not have_selector 'textarea'
+        end
+      end
+
+      scenario 'can add link when editing answer' do
+        within "#answer-#{answer.id}" do
+          click_on 'Add link'
+          fill_in 'Link name', with: 'New link name'
+          fill_in 'Url', with: link_url
+          click_on 'Save answer'
+
+          expect(page).to have_link 'New link name', href: link_url
+        end
+      end
+
+      scenario 'edit his answer with attached files' do
+        within "#answer-#{answer.id}" do
+          fill_in 'Your answer', with: 'edited answer'
+          attach_file 'Files', ["#{Rails.root}/spec/rails_helper.rb", "#{Rails.root}/spec/spec_helper.rb"]
+          click_on 'Save answer'
+
+          expect(page).to have_link 'rails_helper.rb'
+          expect(page).to have_link 'spec_helper.rb'
+        end
+      end
+
+      scenario 'edits his answer with errors' do
+        within "#answer-#{answer.id}" do
+          fill_in 'Your answer', with: ''
+          click_on 'Save answer'
+
+          expect(page).to have_content answer.body
+          expect(page).to have_content "Body can't be blank"
+          expect(page).to have_selector 'textarea'
+        end
+      end
     end
 
     scenario "tries to edit other users's question" do
