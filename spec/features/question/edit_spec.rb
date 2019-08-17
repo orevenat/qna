bfc89ab39b85304f6e799aae85cdf7280840c9b4
Ruslan Knyazev <agrry@yandex.ru>
2019-08-17 16:55:19 +0500

    user can add links when he edit his question and asnwer

--- spec/features/question/edit_spec.rb
+++ spec/features/question/edit_spec.rb
@@ -10,6 +10,7 @@ feature 'User can edit his question', %q{
   given!(:another_user) { create(:user) }
   given!(:question) { create(:question, user: user) }
   given!(:question2) { create(:question, user: another_user ) }
+  given(:link_url) { 'https://google.com' }
   given!(:answer) { create(:answer, question: question, user: user) }
 
   scenario 'Unathenticated can no edit question' do
@@ -23,53 +24,64 @@ feature 'User can edit his question', %q{
       visit question_path(question)
     end
 
-    scenario 'edits his question' do
-      click_on 'Edit question'
-
-      within "#question-area" do
-        fill_in 'Your title', with: 'edited title'
-        fill_in 'Your question', with: 'edited question'
-        click_on 'Save question'
-
-        expect(page).to_not have_content answer.body
-        expect(page).to have_content 'edited title'
-        expect(page).to have_content 'edited question'
-        expect(page).to_not have_selector 'textarea'
+    context 'his own question' do
+      background do
+        click_on 'Edit question'
       end
-    end
 
-    scenario 'edit his question with attached files' do
-      click_on 'Edit question'
+      scenario 'edits his question' do
+        within '#question-area' do
+          fill_in 'Your title', with: 'edited title'
+          fill_in 'Your question', with: 'edited question'
+          click_on 'Save question'
 
-      within "#question-area" do
-        fill_in 'Your title', with: 'edited title'
-        fill_in 'Your question', with: 'edited question'
-        attach_file 'Files', ["#{Rails.root}/spec/rails_helper.rb", "#{Rails.root}/spec/spec_helper.rb"]
-        click_on 'Save question'
-
-        expect(page).to have_link 'rails_helper.rb'
-        expect(page).to have_link 'spec_helper.rb'
+          expect(page).to_not have_content answer.body
+          expect(page).to have_content 'edited title'
+          expect(page).to have_content 'edited question'
+          expect(page).to_not have_selector 'textarea'
+        end
       end
-    end
 
-    scenario 'edits his answer with errors' do
-      click_on 'Edit question'
+      scenario 'can add link when editing question' do
+        within '#question-area' do
+          click_on 'Add link'
+          fill_in 'Link name', with: 'New link name'
+          fill_in 'Url', with: link_url
+          click_on 'Save question'
 
-      within "#question-area" do
-        fill_in 'Your question', with: ''
-        click_on 'Save question'
+          expect(page).to have_link 'New link name', href: link_url
+        end
+      end
 
-        expect(page).to have_content question.body
-        expect(page).to have_content "Body can't be blank"
-        expect(page).to have_selector 'textarea'
+      scenario 'edit his question with attached files' do
+        within '#question-area' do
+          fill_in 'Your title', with: 'edited title'
+          fill_in 'Your question', with: 'edited question'
+          attach_file 'Files', ["#{Rails.root}/spec/rails_helper.rb", "#{Rails.root}/spec/spec_helper.rb"]
+          click_on 'Save question'
+
+          expect(page).to have_link 'rails_helper.rb'
+          expect(page).to have_link 'spec_helper.rb'
+        end
+      end
+
+      scenario 'edits his answer with errors' do
+        within '#question-area' do
+          fill_in 'Your question', with: ''
+          click_on 'Save question'
+
+          expect(page).to have_content question.body
+          expect(page).to have_content "Body can't be blank"
+          expect(page).to have_selector 'textarea'
+        end
       end
     end
 
     scenario "tries to edit other users's question" do
       visit question_path(question2)
 
-      within "#question-area" do
-        expect(page).to_not have_link  'Edit question'
+      within '#question-area' do
+        expect(page).to_not have_link 'Edit question'
       end
     end
   end
