44c8c938922243c8a5fcabbb87511f8c5b2bd42c
Ruslan Knyazev <agrry@yandex.ru>
2019-08-18 13:44:03 +0500

    user can view list of his own rewards

---
+++ spec/controllers/rewards_controller_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe RewardsController, type: :controller do
+  describe 'GET #index' do
+    let(:user) { create(:user) }
+    let(:rewards) { create_list(:reward, 3, user: user) }
+
+    context 'authenticated user' do
+      before do
+        login(user)
+        get :index
+      end
+
+      it 'populates an array of all questions' do
+        expect(assigns(:rewards)).to match_array(rewards)
+      end
+
+      it 'renders index view' do
+        expect(response).to render_template :index
+      end
+
+      it 'returns http success' do
+        expect(response).to have_http_status(:success)
+      end
+    end
+  end
+end
---
+++ spec/features/rewards/index_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+feature 'user can watch his rewards list', %q{
+  As authenticated user
+  I'd like to be able to watch list of my rewards
+} do
+  given(:user) {create(:user)}
+  given(:another_user) { create(:user) }
+  given!(:rewards) { create(:reward, user: user) }
+
+  context 'Authenticated user ' do
+    background do
+      sign_in(user)
+      visit rewards_path
+    end
+
+    scenario 'tries to watch rewards list' do
+      expect(page).to have_content rewards.title
+    end
+  end
+
+  context 'User that not have rewards yet' do
+    background do
+      sign_in(another_user)
+      visit rewards_path
+    end
+
+    scenario ' try to watch rewards list' do
+      expect(page).to have_content 'Your rewards'
+      expect(page).to_not have_content rewards.title
+    end
+  end
+
+  scenario 'Unauthenticated user tries to watch rewards list' do
+    visit rewards_path
+
+    expect(page).to_not have_content rewards.title
+    expect(page).to have_content 'You need to sign in or sign up before continuing.'
+  end
+end
