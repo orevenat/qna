6c8947f2b0238de64d2deff6d34ae620e27e42bc
Ruslan Knyazev <agrry@yandex.ru>
2019-08-18 13:44:03 +0500

    user can view list of his own rewards

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,7 @@ Rails.application.routes.draw do
     end
   end
 
+  resources :rewards, only: :index
+
   root to: 'questions#index'
 end
--- spec/factories/rewards.rb
+++ spec/factories/rewards.rb
@@ -2,5 +2,6 @@ FactoryBot.define do
   factory :reward do
     title { 'My reward' }
     image { fixture_file_upload(Rails.root.join('public', 'reward_logo.png'), 'reward_logo.png') }
+    question { association(:question) }
   end
 end
