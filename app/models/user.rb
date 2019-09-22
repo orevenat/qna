f1a69eea1ae00e4eb74ba08609d7da2b472e4c97
Ruslan Knyazev <agrry@yandex.ru>
2019-09-22 21:50:48 +0500

    user can add comment to answer and question

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :questions, dependent: :destroy
   has_many :answers, dependent: :destroy
+  has_many :comments, dependent: :destroy
   has_many :rewards
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
