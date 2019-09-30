bcfa5259761fd0c9342e0128e00f82e82586216b
Ruslan Knyazev <agrry@yandex.ru>
2019-09-30 19:36:52 +0500

    lesson part

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,26 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
   has_many :questions, dependent: :destroy
   has_many :answers, dependent: :destroy
   has_many :comments, dependent: :destroy
+  has_many :authorizations, dependent: :destroy
   has_many :rewards
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+        :omniauthable, omniauth_providers: [:github]
+
+  def self.find_for_oauth(auth)
+    Services::FindForOauth.new(auth).call
+  end
 
   def author_of?(resource)
     resource.user_id == id
+  end
+
+  def create_authorization(auth)
+    authorizations.create(provider: auth.provider, uid: auth.uid)
   end
 end
