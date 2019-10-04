4fa32f16aa0aed12ef8f3f3cf69ed4443da81bfa
Ruslan Knyazev <agrry@yandex.ru>
2019-10-04 20:35:19 +0500

    first api

---
+++ spec/factories/oauth_applications.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :oauth_applications, class: 'Doorkeeper::Application' do
+    name { 'Test' }
+    redirect_uri { 'urn:ietf:wg:oauth:2.0:oob' }
+    uid { '12345678' }
+    secret { '8751231412' }
+  end
+end
