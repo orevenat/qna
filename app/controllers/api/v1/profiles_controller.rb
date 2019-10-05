2544ac9706dd6136b40fb3224b0c128ed81b68c7
Ruslan Knyazev <agrry@yandex.ru>
2019-10-05 14:05:49 +0500

    add user profile serializer and other user profiles index

--- app/controllers/api/v1/profiles_controller.rb
+++ app/controllers/api/v1/profiles_controller.rb
@@ -3,6 +3,11 @@
 class Api::V1::ProfilesController < Api::V1::BaseController
   skip_authorization_check
 
+  def index
+    @users = User.where.not(id: current_resource_owner.id)
+    render json: @users
+  end
+
   def me
     render json: current_resource_owner
   end
