69c59a0588fd5fefbae184d3d1932330584b4da0
Ruslan Knyazev <agrry@yandex.ru>
2019-10-05 21:03:00 +0500

    add api answer create update destroy

--- spec/api/v1/answers_spec.rb
+++ spec/api/v1/answers_spec.rb
@@ -1,8 +1,9 @@
 require 'rails_helper'
 
 describe 'Answers API', type: :request do
+  let(:me) { create(:user) }
   let(:headers) { { "ACCEPT" => "application/json"} }
-  let(:access_token) { create(:access_token) }
+  let(:access_token) { create(:access_token, resource_owner_id: me.id) }
   let(:params) { { access_token: access_token.token } }
 
   describe 'GET /api/v1/answers/:id' do
@@ -68,6 +69,102 @@ describe 'Answers API', type: :request do
         it 'return url fields for question files' do
           expect(answer_response['files'].first['url']).to eq rails_blob_path(files.first, only_path: true)
         end
+      end
+    end
+  end
+
+  describe 'POST /api/v1/questions/:question_id/answers' do
+    let(:question) { create(:question) }
+    let(:api_path) { "/api/v1/questions/#{question.id}/answers" }
+    let(:answer_params) { { body: "New body" } }
+    let(:params) { { access_token: access_token.token, answer: answer_params } }
+
+    it_behaves_like 'API Authorizable' do
+      let(:method) { 'post' }
+    end
+
+    context 'authorized' do
+      context 'create with valid params' do
+        before { post api_path, params: params, headers: headers }
+
+        it_behaves_like 'Request sucessfull status'
+
+        it 'saves a new question in the database' do
+          expect(me.answers.count).to eq 1
+        end
+
+        it 'return all public fields' do
+          %w[title body].each do |attr|
+            expect(:question_response[attr]).to eq answer_params[attr]
+          end
+        end
+      end
+
+      context 'create with invalid params' do
+        let(:params) { { access_token: access_token.token,
+                         answer: { body: nil } } }
+
+        before { post api_path, headers: headers, params: params }
+
+        it 'return status :unprocessable_entity' do
+          expect(response.status).to eq 422
+        end
+
+        it 'saves a new question in the database' do
+          expect(me.answers.count).to eq 0
+        end
+
+        it 'return error message' do
+          expect(json['errors']).to be_truthy
+        end
+      end
+    end
+  end
+
+  describe 'PATCH /api/v1/answers/:id' do
+    let(:answer_params) { { body: "New body" } }
+    let(:params) { { access_token: access_token.token, answer: answer_params } }
+    let(:answer) { create(:answer, user: me) }
+    let(:api_path) { "/api/v1/answers/#{answer.id}" }
+
+    it_behaves_like 'API Authorizable' do
+      let(:method) { 'patch' }
+    end
+
+    context 'authorized' do
+      context 'update with valid params' do
+        before { patch api_path, headers: headers, params: params }
+
+        it_behaves_like 'Request sucessfull status'
+      end
+
+      context 'update with invalid params' do
+        let(:params) { { access_token: access_token.token,
+                         answer: { body: nil } } }
+
+        before { patch api_path, headers: headers, params: params }
+
+        it 'return status :unprocessable_entity' do
+          expect(response.status).to eq 422
+        end
+      end
+    end
+  end
+
+  describe 'DESTROY /api/v1/answers/:id' do
+    let(:params) { { access_token: access_token.token } }
+    let(:answer) { create(:answer, user: me) }
+    let(:api_path) { "/api/v1/answers/#{answer.id}" }
+
+    it_behaves_like 'API Authorizable' do
+      let(:method) { 'delete' }
+    end
+
+    context 'authorized' do
+      context 'update with valid params' do
+        before { delete api_path, headers: headers, params: params }
+
+        it_behaves_like 'Request sucessfull status'
       end
     end
   end
