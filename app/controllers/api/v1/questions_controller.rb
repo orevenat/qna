675d2013750a51eafbd8c95f0f1f1b4e8aa4f3d2
Ruslan Knyazev <agrry@yandex.ru>
2019-10-05 17:35:09 +0500

    add api show question

--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -1,10 +1,25 @@
 # frozen_string_literal: true
 
 class Api::V1::QuestionsController < Api::V1::BaseController
+  before_action :question, only: [:show]
+
   authorize_resource
 
   def index
-    @questions = Question.all
-    render json: @questions
+    render json: questions
+  end
+
+  def show
+    render json: question
+  end
+
+  private
+
+  def question
+    @question ||= Question.with_attached_files.find(params[:id])
+  end
+
+  def questions
+    @questions ||= Question.all
   end
 end
