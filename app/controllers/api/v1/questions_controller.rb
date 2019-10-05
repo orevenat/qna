265736d6965d98103fd1431331fe232ddb24303e
Ruslan Knyazev <agrry@yandex.ru>
2019-10-05 12:34:46 +0500

    add question serializer

---
+++ app/controllers/api/v1/questions_controller.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+class Api::V1::QuestionsController < Api::V1::BaseController
+  skip_authorization_check
+
+  def index
+    @questions = Question.all
+    render json: @questions
+  end
+end
