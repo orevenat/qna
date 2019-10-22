7b02d66af30f27385861d484323975b58b24e7ea
Ruslan Knyazev <agrry@yandex.ru>
2019-10-22 21:58:50 +0500

    add caching on question page

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,7 +2,7 @@
 
 class Vote < ApplicationRecord
   belongs_to :user
-  belongs_to :votable, polymorphic: true
+  belongs_to :votable, polymorphic: true, touch: true
 
   validates :value, presence: true, inclusion: { in: [-1, 0, 1] }
   validates :user_id, uniqueness: { scope: %i[votable_id votable_type] }
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,6 +28,7 @@ Rails.application.configure do
 
     config.cache_store = :null_store
   end
+  config.action_controller.enable_fragment_cache_logging = true
 
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :amazon
